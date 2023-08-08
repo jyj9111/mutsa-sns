e13fb612253f884e34b64ad8c973470472eefbb6
jyj9111 <mrjang9111@gmail.com>
2023-08-08 14:45:32 +0900

    Fix: HttpStatus-Code 변경  - 400: Bad Request -> 404: Not Found

--- src/main/java/com/mutsa/sns/domain/article/exception/ArticleNotExist.java
+++ src/main/java/com/mutsa/sns/domain/article/exception/ArticleNotExist.java
@@ -1,8 +1,8 @@
 package com.mutsa.sns.domain.article.exception;
 
-import com.mutsa.sns.global.error.status.Status400Exception;
+import com.mutsa.sns.global.error.status.Status404Exception;
 
-public class ArticleNotExist extends Status400Exception {
+public class ArticleNotExist extends Status404Exception {
     public ArticleNotExist() {
         super("해당 게시글(피드)가 존재하지 않습니다.");
     }
