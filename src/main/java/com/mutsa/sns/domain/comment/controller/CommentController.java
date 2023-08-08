ef42b06286328c199effec99d5486ebc2d951058
jyj9111 <mrjang9111@gmail.com>
2023-08-08 10:59:55 +0900

    Feat: 댓글 등록 기능 추가 - 로그인 한 유저만 등록가능 - 응답:  - 댓글 id  - 작성자 이름  - 댓글 내용

--- src/main/java/com/mutsa/sns/domain/comment/controller/CommentController.java
+++ src/main/java/com/mutsa/sns/domain/comment/controller/CommentController.java
@@ -1,4 +1,27 @@
 package com.mutsa.sns.domain.comment.controller;
 
+import com.mutsa.sns.domain.comment.dto.CommentRequestDto;
+import com.mutsa.sns.domain.comment.dto.CommentResponseDto;
+import com.mutsa.sns.domain.comment.service.CommentService;
+import com.mutsa.sns.global.common.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/comments")
+@RequiredArgsConstructor
 public class CommentController {
+    private final CommentService commentService;
+
+    // 댓글 등록
+    @PostMapping("/{article_id}")
+    public CommentResponseDto create(
+            Principal principal,
+            @PathVariable("article_id") Long articleId,
+            @RequestBody CommentRequestDto dto
+    ) {
+       return commentService.createComment(principal.getName(), articleId, dto);
+    }
 }
